e5b9f5a30f1676055ae78456a1e9f3142131f616
sahlminkok <sahlminkok7@gmail.com>
2023-06-23 06:57:02 +0100

    Replace the category input with author

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -2,11 +2,7 @@ const InputBook = () => (
   <>
     <form>
       <input placeholder="Book title" />
-      <select name="Category">
-        <option value="">Category</option>
-        <option value="science-fiction">Science Fiction</option>
-        <option value="action">Action</option>
-      </select>
+      <input placeholder="Author" />
       <button type="button">Add Book</button>
     </form>
   </>
