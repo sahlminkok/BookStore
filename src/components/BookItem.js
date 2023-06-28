56b34e1004281cca9fae73ffeb2380b452eecbbf
sahlminkok <sahlminkok7@gmail.com>
2023-06-28 06:50:08 +0100

    Update the structuce of the app

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,30 +1,49 @@
 import PropTypes from 'prop-types';
 
-const BookItem = ({ itemProp, deleteBook }) => (
-  <li>
+const BookItem = ({ itemProp }) => (
+  <div>
     <div>
       <p>{itemProp.category}</p>
-      <h2>{itemProp.name}</h2>
+      <h2>{itemProp.title}</h2>
       <p>{itemProp.author}</p>
     </div>
+    <ul>
+      <li>
+        <button type="button">Comments</button>
+      </li>
+      <li>
+        <button type="button">
+          Remove
+        </button>
+      </li>
+      <li>
+        <button type="button">Edit</button>
+      </li>
+    </ul>
     <div>
-      <button type="button">Comments</button>
-      <button type="button" onClick={() => deleteBook(itemProp.id)}>
-        Remove
-      </button>
-      <button type="button">Edit</button>
+      <p>
+        {itemProp.completed}
+        %
+      </p>
+      <p>completed</p>
     </div>
-  </li>
+    <div>
+      <p>current chapter</p>
+      <p>{itemProp.chapter}</p>
+      <button type="button">update progress</button>
+    </div>
+  </div>
 );
 
 BookItem.propTypes = {
   itemProp: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
+    id: PropTypes.number,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    category: PropTypes.string,
+    completed: PropTypes.number,
+    chapter: PropTypes.string,
   }).isRequired,
-  deleteBook: PropTypes.func.isRequired,
 };
 
 export default BookItem;
