ba7de57aa79714d3cb72990d97c8a270ccb1d912
sahlminkok <sahlminkok7@gmail.com>
2023-07-01 15:50:45 +0100

    Remove commented code

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -48,21 +48,7 @@ const initialState = {
 const booksSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    // addBook: (state, action) => {
-    //   const newBook = {
-    //     id: uuidv4(),
-    //     title: action.payload[0],
-    //     category: 'Science',
-    //     author: action.payload[1],
-    //   };
-    //   state.booksList = [...state.booksList, newBook];
-    // },
-    // removeBook: (state, action) => {
-    //   const bookId = action.payload;
-    //   state.booksList = state.booksList.filter((book) => book.id !== bookId);
-    // },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(getBooks.pending, (state) => {
